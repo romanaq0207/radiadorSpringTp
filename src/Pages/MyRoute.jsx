9e80cf05b1cff40b63774bd4d6a0fbb383012ec3
santiago gonzalez <leosag2507@gmail.com>
2024-09-21 15:44:32 -0300

    small fix NavBar

--- src/Pages/MyRoute.jsx
+++ src/Pages/MyRoute.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ejemplo from "./images/ejemplo-map.jpg";
+import Navbar from "../components/NavBar";
 import "./MyRoute.css";
 
 function MyRoute() {
   return (
     <div>
+      <Navbar />
       <img id="img" src={ejemplo} />
     </div>
   );
