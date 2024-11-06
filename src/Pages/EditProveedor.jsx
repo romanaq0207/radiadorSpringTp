86de38f47efb88cc1386f3a547c7a8dfdbc813cd
Fiosaucedo <fiorellasaucedo@hotmail.com>
2024-11-06 08:07:36 -0300

    edita proveedor ok

--- src/Pages/EditProveedor.jsx
+++ src/Pages/EditProveedor.jsx
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFloppyDisk,faBan} from '@fortawesome/free-solid-svg-icons';
+import { faFloppyDisk, faBan } from '@fortawesome/free-solid-svg-icons';
 import "./EditProveedor.css";
 import { API_BASE_URL } from "../assets/config"; // Asegúrate de que esta ruta sea correcta
-import Swal from 'sweetalert2'; 
+import Swal from 'sweetalert2';
 
 const EditProveedor = () => {
   const { id } = useParams();
   const [proveedor, setProveedor] = useState(null);
+  const [originalCuil, setOriginalCuil] = useState(""); // Estado para el CUIT original
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
 
@@ -17,9 +18,9 @@ const EditProveedor = () => {
     const fetchProveedor = async () => {
       try {
         const response = await axios.get(`${API_BASE_URL}/proveedores/${id}`);
-        console.log('URL de la solicitud GET:', `${API_BASE_URL}/proveedores/${id}`);
         if (response.data) {
           setProveedor(response.data);
+          setOriginalCuil(response.data.cuil); // Guardar el CUIT original
         }
       } catch (error) {
         console.error("Error al obtener el proveedor de la API:", error);
@@ -34,6 +35,7 @@ const EditProveedor = () => {
     const cuilRegex = /^\d{2}-\d{8}-\d{1}$/;
     const addressRegex = /^[A-Za-z0-9\s,]+$/;
     const phoneRegex = /^\d{8}$|^\d{10}$/;
+    
     if (!proveedor.nombre || !nameRegex.test(proveedor.nombre)) {
       newErrors.nombre = "El nombre solo debe contener letras y espacios.";
     }
@@ -61,23 +63,46 @@ const EditProveedor = () => {
     event.preventDefault();
     if (validate()) {
       try {
+        // Verificar si el CUIT fue modificado
+        if (proveedor.cuil !== originalCuil) {
+          // Validar existencia solo si el CUIT es diferente al original
+          const response = await axios.get(`${API_BASE_URL}/proveedores/cuil/${proveedor.cuil}`);
+          const proveedorExistente = response.data;
+  
+          if (proveedorExistente) {
+            await Swal.fire({
+              title: 'CUIT existente',
+              text: 'Ya existe otro proveedor con este CUIT.',
+              icon: 'warning',
+              confirmButtonText: 'Aceptar',
+            });
+            return; // No procede con la edición si existe otro proveedor con el mismo CUIT
+          }
+        }
+  
+        // Si no hay duplicados o el CUIT no cambió, proceder con la actualización
         const url = `${API_BASE_URL}/proveedores/modificar-proveedor/${id}`;
-        console.log('URL de la solicitud PUT:', url);
         await axios.put(url, proveedor);
-        console.log("Cambios guardados:", proveedor);
+        
         Swal.fire({
           title: '¡Actualización exitosa!',
           text: 'La información del proveedor se ha actualizado correctamente.',
           icon: 'success',
-          confirmButtonText: 'Aceptar'
+          confirmButtonText: 'Aceptar',
         });
-        navigate("/gestion-proveedores"); // Redirect after saving
+        navigate("/gestion-proveedores"); // Redirigir después de guardar
       } catch (error) {
         console.error("Error al guardar los cambios del proveedor:", error);
+        Swal.fire({
+          title: "¡Error!",
+          text: "Hubo un problema al guardar los cambios.",
+          icon: "error",
+          confirmButtonText: "Aceptar",
+        });
       }
     }
-  };  
-  
+  };
+
   if (!proveedor) {
     return <p>Proveedor no encontrado</p>;
   }
@@ -153,9 +178,10 @@ const EditProveedor = () => {
           {errors.telefono && <span className="error">{errors.telefono}</span>}
         </div>
         <button type="submit" className="submit-button">
-        <FontAwesomeIcon icon={faFloppyDisk} style={{color: "#ffffff",}} />        </button>
+          <FontAwesomeIcon icon={faFloppyDisk} style={{ color: "#ffffff" }} />
+        </button>
         <button onClick={handleBack} id="btn-cancelar">
-        <FontAwesomeIcon icon={faBan} style={{color: "#ffffff",}} />
+          <FontAwesomeIcon icon={faBan} style={{ color: "#ffffff" }} />
         </button>
       </form>
     </div>
